d258e0f75e1a225af80c600e25d72c3c2686e777
unknown <okediran96@gmail.com>
2022-01-26 03:35:57 -0800

    menubar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -12,39 +12,41 @@ export default function Navbar() {
 
     return (
          <div className="header">   
-            <nav class="navbar nav1 navbar-expand-lg">
-                <div class="container-fluid">
+            <nav className="navbar nav1 navbar-expand-lg">
+                <div className="container-fluid">
                     <div className="nav-header d-flex">
-                        <Link to="" class="navbar-brand d-flex"><img className='justify-content-center' src={img} alt="logo"/>{bar?<h3>ZENIX</h3>:null}</Link>
-                        <button class="btn" onClick={iconChange} type="button">
-                            <span class={bar?"fa fa-bars":"fa fa-arrow-right"}></span>
+                        <Link to="" className="navbar-brand d-flex"><img className='justify-content-center' src={img} alt="logo"/>
+                            {bar?<h3>ZENIX</h3>:null}
+                        </Link>
+                        <button className="btn" onClick={iconChange} type="button">
+                            <span className={bar?"fa fa-align-right":"fa fa-arrow-right"}></span>
                         </button>
                     </div>
-                    <form  class="d-flex bg-light">
-                        <input class="form-control me-2 bg-light" type="search" placeholder="Search" aria-label="Search"/>
+                    <form  className="d-flex bg-light">
+                        <input className="form-control me-2 bg-light" type="search" placeholder="Search" aria-label="Search"/>
                         <i className="fa fa-search"></i>
                     </form>
                     <div id="navbarSupportedContent">
-                        <ul class="navbar-nav  mb-lg-0">
-                            <li class="nav-item">
+                        <ul className="navbar-nav  mb-lg-0">
+                            <li className="nav-item">
                                 <Link to="" className="nav-link"><i className="fa fa-moon-o"></i></Link>
                             </li>
-                            <li class="nav-item dropdown">
+                            <li className="nav-item dropdown">
                                <Link to="" className="nav-link"><i className="fa fa-bell-o"></i></Link> 
-                               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><Link class="dropdown-item" to="#">Action</Link></li>
-                                    <li><Link class="dropdown-item" to="#">Another action</Link></li>
-                                    <li><hr class="dropdown-divider"/></li>
-                                    <li><Link class="dropdown-item">Something else here</Link></li>
+                               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><Link to="" className="dropdown-item" to="#">Action</Link></li>
+                                    <li><Link to="" className="dropdown-item" to="#">Another action</Link></li>
+                                    <li><hr className="dropdown-divider"/></li>
+                                    <li><Link to="" className="dropdown-item">Something else here</Link></li>
                                 </ul>
                             </li>
-                            <li class="nav-item">
+                            <li className="nav-item">
                                <Link to="" className="nav-link"><i className="fa fa-comment-o"></i></Link> 
                             </li>
-                            <li class="nav-item">
+                            <li className="nav-item">
                                 <Link to="" className="nav-link"><i className="fa fa-gift"></i></Link>
                             </li>
-                            <li class="nav-item li-last">
+                            <li className="nav-item li-last">
                                 <Link to="" className="nav-link last "> 
                                 <   Avatar className="avater" shape="square" size={44} icon={<UserOutlined />} />
                                     <div><span>Johndoe</span> <br/>
@@ -55,20 +57,20 @@ export default function Navbar() {
                     </div>
                 </div>
             </nav>
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="container-fluid">
-                    <p class="navbar-brand" to="#">Dashboard</p>
-                    <ul class="navbar-nav mb-lg-0">
-                        <li class="nav-item btn-link">
-                            <Link to="" class="btn">Today</Link> 
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container-fluid">
+                    <p className={bar?"navbar-brand brand2":"navbar-brand show"}  to="#">Dashboard</p>
+                    <ul className="navbar-nav mb-lg-0">
+                        <li className="nav-item btn-link">
+                            <Link to="" className="btn">Today</Link> 
                         </li>
-                        <li class="nav-item btn-link">
-                            <Link to="" class="btn">Month</Link>
+                        <li className="nav-item btn-link">
+                            <Link to="" className="btn">Month</Link>
                        </li>
-                            <li class="nav-item dropdown">         
+                            <li className="nav-item dropdown">         
                         </li>
-                        <li class="nav-item btn-link">
-                            <Link to="" class="btn ">Year</Link>
+                        <li className="nav-item btn-link">
+                            <Link to="" className="btn ">Year</Link>
                         </li>
                     </ul>
                     </div>
